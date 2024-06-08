03312827c5661454919e857741f9a2952bdb28f6
ignatiusm <ignatius.menzies@gmail.com>
2024-06-08 22:45:40 +1200

    Compiles and runs

--- hpci/app/Main.hs
+++ hpci/app/Main.hs
@@ -1,20 +1,22 @@
 module Main (main) where
 
 import Network.SSH.Client.SimpleSSH
-import Control.Monad.Trans.Except (runErrorT)
+import Control.Monad.Except
 
 main :: IO ()
 main = do
-  let hostname = "gadi.nci.org.au"
+  let hostname = "hostname"
       port = 22
-      username = "im9095"
+      username = "abc123"
       publicKey = "~/.ssh/id_rsa.pub"
+      privateKey = "~/.ssh/id_rsa"
+      knownHosts = "~/.ssh/known_hosts"
 
-  result <- runErrorT $ do 
-    session <- openSession hostname port
-    authenticateWithPublicKeyFile session username publicKey
-    runCommand session "ls -l"
+  result <- runExceptT $ do
+    session <- openSession hostname port knownHosts
+    _ <- authenticateWithKey session username publicKey privateKey ""
+    execCommand session "ls -l"
 
   case result of
     Left err -> putStrLn $ "SSH Error: " ++ show err
-    Right output -> putStrLn output
+    Right output -> putStrLn $ show output

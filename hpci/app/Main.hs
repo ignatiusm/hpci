b32fda9464ee006f7e66bdeebdbf3aeaa34508b7
ignatiusm <ignatius.menzies@gmail.com>
2024-06-14 16:05:53 +1200

    Move to libssh-hs

--- hpci/app/Main.hs
+++ hpci/app/Main.hs
@@ -1,22 +1,26 @@
 module Main (main) where
 
-import Network.SSH.Client.SimpleSSH
-import Control.Monad.Except
+import qualified Data.ByteString.Lazy as BSL
 
-main :: IO ()
+import Network.SSH.Client.LibSSH2.Foreign
+import Network.SSH.Client.LibSSH2
+import System.Environment
+import System.FilePath
+
 main = do
-  let hostname = "hostname"
-      port = 22
-      username = "abc123"
-      publicKey = "~/.ssh/id_rsa.pub"
-      privateKey = "~/.ssh/id_rsa"
-      knownHosts = "~/.ssh/known_hosts"
+  args <- getArgs
+  case args of 
+    [user, host, port, cmd, knownHost, public, private] -> runCommand user host (read port) cmd knownHost public private
+    _ -> putStrLn "run with following args: username hostname port cmd knownHosts publickey privatekey"
 
-  result <- runExceptT $ do
-    session <- openSession hostname port knownHosts
-    _ <- authenticateWithKey session username publicKey privateKey ""
-    execCommand session "ls -l"
+ssh login host port knownHost public private actions = do
+  initialize True
+  withSSH2 knownHost public private "" login host port $ actions
+  exit
 
-  case result of
-    Left err -> putStrLn $ "SSH Error: " ++ show err
-    Right output -> putStrLn $ show output
+runCommand user host port command knownHost public private =
+  ssh user host port knownHost public private $ \s ->
+    withChannel s $ \ch -> do
+      channelExecute ch command
+      result <- readAllChannel ch
+      BSL.putStr result

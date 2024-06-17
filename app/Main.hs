eea39f63fc7c9ee7fdea6dc21e77f2bc130993ba
ignatiusm <ignatius.menzies@gmail.com>
2024-06-17 21:05:23 +1200

    Adds file send

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import qualified Data.ByteString.Lazy as BSL
 import System.Environment
+import System.FilePath
 
 import Network.SSH.Client.LibSSH2.Foreign
 import Network.SSH.Client.LibSSH2
@@ -10,8 +11,8 @@ main :: IO()
 main = do
   args <- getArgs
   case args of 
-    [user, host, port, cmd, knownHost, public, private] -> runCommand user host (read port) cmd knownHost public private
-    _ -> putStrLn "run with following args: username hostname port cmd knownHosts publickey privatekey"
+    [user, host, port, cmd, knownHost, public, private, script] -> runCommand user host (read port) cmd knownHost public private script
+    _ -> putStrLn "run with following args: username hostname port cmd knownHosts publickey privatekey script"
 
 ssh :: String -> String -> Int -> FilePath -> FilePath -> FilePath -> (Session -> IO a) -> IO()
 ssh login host port knownHost public private actions = do
@@ -19,9 +20,11 @@ ssh login host port knownHost public private actions = do
   withSSH2 knownHost public private "" login host port $ actions
   exit
 
-runCommand :: String -> String -> Int -> String -> FilePath -> FilePath -> FilePath -> IO()
-runCommand user host port command knownHost public private =
-  ssh user host port knownHost public private $ \s ->
+runCommand :: String -> String -> Int -> String -> FilePath -> FilePath -> FilePath -> FilePath -> IO()
+runCommand user host port command knownHost public private script =
+  ssh user host port knownHost public private $ \s -> do
+    sz <- scpSendFile s 0o644 script (takeFileName script)
+    putStrLn $ "Sent: " ++ show sz ++ " bytes."
     withChannel s $ \ch -> do
       channelExecute ch command
       result <- readAllChannel ch

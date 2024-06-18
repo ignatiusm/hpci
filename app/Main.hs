16698db1abb5a017d11cdc2fbc9408f4a637d160
ignatiusm <ignatius.menzies@gmail.com>
2024-06-18 14:37:46 +1200

    Refactor to steps within session

--- app/Main.hs
+++ app/Main.hs
@@ -7,25 +7,43 @@ import System.FilePath
 import Network.SSH.Client.LibSSH2.Foreign
 import Network.SSH.Client.LibSSH2
 
+runHpci :: String -> String -> Int -> String -> FilePath -> FilePath -> FilePath -> FilePath -> IO ()
+runHpci user host port command knownHost public private script = do
+  --  Initialize session
+  session <- sessionInit host port
+  putStrLn "Start Session"
+
+  -- Check remote host against known hosts list
+  _ <- checkHost session host port knownHost [TYPE_MASK]
+
+  -- Authenticate
+  publicKeyAuthFile session user public private ""
+  putStrLn "Authorised"
+
+  -- Send a file to remote host via SCP.
+  sz <- scpSendFile session 0o644 script (takeFileName script)
+  putStrLn $ "Sent: " ++ show sz ++ " bytes."
+
+  -- Execute some actions within SSH2 channel
+  _ <- withChannel session $ \ch -> do
+         channelExecute ch (command ++ " " ++ script)
+         result <- readAllChannel ch
+         BSL.putStr result
+
+  -- TODO: poll status of job, until error or finished (with timeout?)
+
+  -- TODO: copy logs file off server to ci
+
+  -- Close active session
+  sessionClose session
+  putStrLn "Closed Session"
+
+  -- TODO: print logs file if there is one
+
+
 main :: IO()
 main = do
   args <- getArgs
   case args of 
-    [user, host, port, cmd, knownHost, public, private, script] -> runCommand user host (read port) cmd knownHost public private script
-    _ -> putStrLn "run with following args: username hostname port cmd knownHosts publickey privatekey script"
-
-ssh :: String -> String -> Int -> FilePath -> FilePath -> FilePath -> (Session -> IO a) -> IO()
-ssh login host port knownHost public private actions = do
-  initialize True
-  withSSH2 knownHost public private "" login host port $ actions
-  exit
-
-runCommand :: String -> String -> Int -> String -> FilePath -> FilePath -> FilePath -> FilePath -> IO()
-runCommand user host port command knownHost public private script =
-  ssh user host port knownHost public private $ \s -> do
-    sz <- scpSendFile s 0o644 script (takeFileName script)
-    putStrLn $ "Sent: " ++ show sz ++ " bytes."
-    withChannel s $ \ch -> do
-      channelExecute ch command
-      result <- readAllChannel ch
-      BSL.putStr result
+    [user, host, port, cmd, knownHost, public, private, script] -> runHpci user host (read port) cmd knownHost public private script
+    _ -> putStrLn "required args: username hostname port cmd knownHosts publickey privatekey script"
